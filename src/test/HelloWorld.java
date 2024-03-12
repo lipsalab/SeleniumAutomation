d84cdce1d5119819b6191edfd321ee20a9c2cf69
lipsalab <lipsadas108@gmail.com>
2024-03-13 02:55:17 +0530

    Again commiting

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,7 +3,17 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-System.out.println("hello world");
+     //System.out.println("hello world");
+      
+     
+     
+     String a="LipsaDas";
+     String b="";
+     for(int i=a.length()-1;i>=0;i--) {
+    	 b=b+a.charAt(i);
+     }
+     System.out.println(b);
+
 	}
 
 }
